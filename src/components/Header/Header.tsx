3a5a31ee341e718fee2b89154800577c752f93f0
kaustubh joshi <joshigikaustubh@gmail.com>
2022-03-05 15:15:31 +0530

    logout changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import './Header.css';
+import { useNavigate } from 'react-router-dom';
 
 const Header : React.FC = () => {
+
+    const [showLogout, setShowLogout] = useState(false);
+    const navigate = useNavigate();
+
+    const logout = () => {
+        window.localStorage.setItem('login','false');
+        setShowLogout(false);
+        navigate("/");
+    }
+
+    useEffect(() => {
+        if(window.localStorage.getItem('login')==='true'){
+            setShowLogout(true);
+        }
+        else{
+            setShowLogout(false);
+        }
+    },[])
     return(
         <div className="HeaderClass">
             <h1 className="headtext">The Warehouse</h1>
-            <button className="buttontext">Logout</button>
+            { 
+                <button className="buttontext" onClick={logout}>Logout</button>
+            }
         </div>
     )
 }
